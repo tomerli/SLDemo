3f3c3416fe183d0daa1768154f45ed684c2046a4
imfaus <ianianf@yahoo.com>
2025-06-17 11:33:45 -0500

    Update Function_more_army_follow.java

--- src/main/java/com/tricentis/swan/station/functions/Function_more_army_follow.java
+++ src/main/java/com/tricentis/swan/station/functions/Function_more_army_follow.java
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-// Function class: Function_more_army_follow - Updated automatically
-=======
-// Updated by GitHub Actions on 2025-06-17T16:26:35Z
->>>>>>> c287a07676ba2376b00f501a6f7f3fe9ae597d37
-// Function updated automatically by script - Fri Jun 13 14:25:44 UTC 2025
+
 package com.tricentis.swan.station.functions;
 
 import org.springframework.stereotype.Component;
@@ -12,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class Function_more_army_follow implements Function {
     @Override
     public String execute() {
-        System.out.println("function_more_army_follow executed"".");;
+        System.out.println("function_more_army_follow executed.");;
         return "function_more_army_follow executed";
     }
 }
